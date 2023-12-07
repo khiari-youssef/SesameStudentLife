7dd27abdca6029d8a8a191ca84a5c8a2b2ae58b1
youssefkh <youssef.khiari@keyrustunisie.onmicrosoft.com>
2023-12-07 11:46:57 +0100

    + changelog.md at ## [1.0.0] - 2023-03-05

--- androidApp/src/main/java/tn/sesame/spm/android/MainActivity.kt
+++ androidApp/src/main/java/tn/sesame/spm/android/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import tn.sesame.spm.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MyApplicationTheme {
                 Surface(
--- changelog.md
+++ changelog.md
@@ -3,7 +3,7 @@
 
 ## [Unreleased]
 
-## [1.1.1] - 2023-03-05
+## [1.0.0] - 2023-03-05
 
 ### Added
 
@@ -12,3 +12,6 @@
 - designsystem module
 - Koib/SqlDelight/Ktor for android target
 - project initialization
+- ManifestFile Configurations
+- Designsystem styles
+- SplashScreen
