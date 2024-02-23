a9450aa023466668b4b67e15537569c3d2c6d2af
YouYou <khiari.youssef98@gmail.com>
2024-02-23 13:29:04 +0100

    + re-organized gradle dependencies using version catalog bundles

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -31,18 +31,8 @@ kotlin {
             implementation(libs.kotlin.test)
         }
         androidMain.dependencies{
-            implementation(libs.compose.ui)
-            implementation(libs.compose.ui.tooling.preview)
-            implementation(libs.compose.material3)
-            implementation(libs.compose.foundation)
+            implementation(libs.bundles.composelibs)
             implementation(libs.androidx.splashscreen)
-            implementation(libs.compose.navigation)
-            implementation(libs.compose.lottie)
-            implementation(libs.compose.animation.core)
-            implementation(libs.compose.animation.graphics)
-            implementation(libs.compose.constraintlayout)
-            implementation(libs.compose.coil)
-            implementation(libs.compose.material)
         }
     }
 }
