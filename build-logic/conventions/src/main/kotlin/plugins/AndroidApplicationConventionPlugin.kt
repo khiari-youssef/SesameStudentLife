a760576bd439e6cb352a360b4c177318a09295b7
YouYou <khiari.youssef98@gmail.com>
2024-02-25 19:37:46 +0100

    + updated keystore debug config

--- build-logic/conventions/src/main/kotlin/plugins/AndroidApplicationConventionPlugin.kt
+++ build-logic/conventions/src/main/kotlin/plugins/AndroidApplicationConventionPlugin.kt
@@ -6,9 +6,18 @@ import libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import java.io.FileInputStream
+import java.util.Properties
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
+        val keystoreProperties = Properties()
+        runCatching {
+            val keystorePropertiesFile = target.file("keystore.properties")
+            keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+        }.onFailure { th->
+            th.printStackTrace()
+        }
         with(target) {
             with(pluginManager){
                 apply("com.android.application")
@@ -22,13 +31,20 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                 packaging.resources.excludes += "/META-INF/versions/9/previous-compilation-data.bin"
                 defaultConfig.versionCode = 1
                 defaultConfig.versionName = "1.0"
-
                 buildTypes {
-                    getByName("debug"){
+                    debug {
                         isDebuggable = true
                         isMinifyEnabled = false
+                        signingConfigs {
+                            create("signingConfig") {
+                                keyAlias = keystoreProperties["keyAlias"].toString()
+                                keyPassword = keystoreProperties["keyPassword"].toString()
+                                storeFile = file(keystoreProperties["storeFile"].toString())
+                                storePassword = keystoreProperties["storePassword"].toString()
+                            }
+                        }
                     }
-                    getByName("release") {
+                    release {
                         isMinifyEnabled = true
                         isShrinkResources = true
                     }
